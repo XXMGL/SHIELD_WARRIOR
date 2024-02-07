97e42a8307d0a5ff0482cfcbf18859a60a2dd925
DearJohnn <sun.yuch@northeastern.edu>
2024-02-07 16:20:43 -0500

    Background and collision
    The background and collision of the game were made using tilemaps. The background is divided into 4 layers, namely ground, wall, UI area and items.

---
+++ Scripts/Character.gd
@@ -0,0 +1,74 @@
+extends CharacterBody2D
+
+@export var MOVE_SPEED = 200 #玩家的移动速度
+@export var SlowDown = 2
+@export var ParryDuration = 0.1
+var motion = Vector2()
+enum {STATE_MOVE, STATE_HURT, STATE_DIE, STATE_PARRYING,  STATE_PARRYSTART, STATE_PARRYEND}
+var state = STATE_MOVE
+var parry_timer = 0.0
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	match state:
+		STATE_MOVE:
+			if Input.is_action_pressed("parry"):
+				state = STATE_PARRYSTART
+				parry_timer = 0.0
+			_MOVE(MOVE_SPEED)
+			pass
+		STATE_PARRYSTART:
+			print("Parry Start")
+			parry_timer += delta
+			if parry_timer >= ParryDuration:
+				state = STATE_PARRYING
+			_MOVE(MOVE_SPEED / SlowDown)
+			pass
+		STATE_PARRYING:
+			print("Parrying")
+			if not Input.is_action_pressed("parry"):
+				state = STATE_PARRYEND
+				parry_timer = 0.0
+			_MOVE(MOVE_SPEED / SlowDown)
+			pass
+		STATE_PARRYEND:
+			print("Parry End")
+			parry_timer += delta
+			if parry_timer >= ParryDuration:
+				state = STATE_MOVE
+			_MOVE(MOVE_SPEED / SlowDown)
+			pass
+		STATE_HURT:
+			pass
+		STATE_DIE:
+			pass
+			
+
+func _MOVE(speed):
+			motion = Vector2(0,0) #初始化玩家的移动向量w	
+			# 检测键盘输入并更新移动向量
+			if Input.is_action_pressed("move_right"):
+				motion.x += 1
+			if Input.is_action_pressed("move_left"):
+				motion.x -= 1
+			if Input.is_action_pressed("move_down"):
+				motion.y += 1
+			if Input.is_action_pressed("move_up"):
+				motion.y -= 1
+				
+			#将移动向量归一化，防止对角线移动速度更快
+			motion = motion.normalized()
+			
+			#将移动向量乘以移动速度乘以delta来实现平滑的移动
+			motion *= speed
+			
+			#将motion变量赋予velocity并且控制玩家移动
+			set_velocity(motion)
+			move_and_slide()			
+	
+	
+	
---
+++ Scripts/Enemy_Type1.gd
@@ -0,0 +1,64 @@
+extends CharacterBody2D
+
+@export var move_speed = 100  # 在x方向的移动速度
+@export var approach_speed = 50  # 在y轴上向玩家靠近的速度
+@export var player_root = "玩家路径" # 玩家路径
+var x_direction = 1
+var x_velocity
+var y_direction = 1
+var y_velocity
+var timer = 0.0
+
+var bullet_tscn = preload("res://TSCN/bullet_1.tscn")
+@export var ShootDuration = 2.0
+var Shoot_timer = 0.0
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	Shoot_timer += delta
+	if Shoot_timer >= ShootDuration:
+		_ShootBullet()
+		Shoot_timer = 0
+	else:
+		pass
+	
+	_FoundTarget(player_root)		
+	# 在x方向上获得随即方向,每0.5秒随机获得方向
+	timer += delta
+	if timer >= 0.5:
+		_ShootBullet()
+		x_direction = randf_range(-1, 1)  # 随机选择 -1、0、1
+		timer = 0
+	x_velocity = x_direction * move_speed
+	
+	# 限制x轴移动范围
+	#global_position.x = global_position.x + x_velocity
+
+	y_velocity = y_direction * approach_speed
+	
+	# 更新y轴位置
+	#global_position.y += y_velocity
+	velocity = Vector2(x_velocity,y_velocity)	
+	move_and_slide()	
+	
+func _FoundTarget(TargetPath):
+	var target = get_parent().get_node(TargetPath)
+	if target != null:
+		var target_position = target.global_position
+		if target_position.y > global_position.y:
+			y_direction = 1
+		elif  target_position.y < global_position.y:
+			y_direction = -1
+		else:
+			y_direction = randf_range(-1, 1)  # 随机选择 -1、0、1
+	else:
+		pass
+
+func _ShootBullet():
+	var bullet = bullet_tscn.instantiate()
+	get_parent().add_child(bullet)
+	bullet.position = $BulletSpawner.global_position
