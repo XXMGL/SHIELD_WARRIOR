91bff4727a9dfcf682d8a681f4112bb76d23a0c4
MaskMan-DU <duyiran19385@gmail.com>
2024-02-28 16:30:57 -0500

    Update Enemy Type 101 move

--- Scripts/Enemy/Enemy_Type101.gd
+++ Scripts/Enemy/Enemy_Type101.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 var tail = preload("res://TSCN/Enemy/Enemies/enemy_type_101_Tail.tscn")
 @onready var tailGenerateTimer = $Tail_Generate_Timer
-var tailGenerateWaitTime = 0
 var tailGenerateDuration = 0.2
 
 @export var move_direction = Node2D
@@ -18,35 +17,24 @@ var prev_dir = Vector2(1,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	tailGenerateTimer.wait_time = tailGenerateDuration
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	tailGenerateWaitTime += delta
-	if (tailGenerateWaitTime > tailGenerateDuration):
-		tailGenerateWaitTime = 0
-		tailGenerateTimer.wait_time = tailGenerateDuration
-		tailGenerateTimer.start()
 			
 	move()
 	move_and_slide()
 	
 func move():
 	velocity = move_direction.direction * move_speed
-	
-	var dir_change = false
-	if (prev_dir != move_direction.direction):
-		prev_dir = move_direction.direction
-		dir_change = true
-	if (dir_change == true):
-		for i in tails:
-			i.add_move_dir_to_tail(position, move_direction.direction)
 			
 func add_tails():
 	if (tails.size() < 6):
 		var new_tail = tail.instantiate()
 		new_tail.move_speed = move_speed
+		new_tail.group_name = group_name
 		get_parent().add_child(new_tail)
 		tails.append(new_tail)
 
--- Scripts/Enemy/Enemy_Type101_Tail.gd
+++ Scripts/Enemy/Enemy_Type101_Tail.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 
-var cur_dir = Vector2(1,0)
-var directions = []
-var pos_array = []
-
 var move_speed = 100
 
+@export var move_direction = Node2D
+
+var group_name : String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,24 +13,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	#print_debug(directions.size())
-	print_debug(pos_array.size())
-	
-	if (directions.size() > 0):
-		if (position == pos_array[0]):
-			cur_dir = directions[0]
-			remove_move_dir_from_tail()
-			
-	print_debug(cur_dir)		
-	velocity = cur_dir * move_speed
+	velocity = move_direction.direction * move_speed
 	
 	move_and_slide()
 	
-func remove_move_dir_from_tail():
-	directions.pop_front()
-	pos_array.pop_front()
-	
-func add_move_dir_to_tail(head_pos, dir):
-	directions.append(dir)
-	pos_array.append(head_pos)
+
 
