4aea98de8c0879343accf1e3c737fefd471b5b4d
MaskMan-DU <duyiran19385@gmail.com>
2024-02-28 16:15:39 -0500

    Add Enemy Type 101

--- Scripts/Enemy/Enemy_Type101.gd
+++ Scripts/Enemy/Enemy_Type101.gd
@@ -1,8 +1,13 @@
-extends Node2D
+extends CharacterBody2D
+
+var tail = preload("res://TSCN/Enemy/Enemies/enemy_type_101_Tail.tscn")
+@onready var tailGenerateTimer = $Tail_Generate_Timer
+var tailGenerateWaitTime = 0
+var tailGenerateDuration = 0.2
 
 @export var move_direction = Node2D
 @export var move_speed = 100
-@export var tails = []
+var tails = []
 
 @export var group_name : String
 @export var ShootDuration = 2.0
@@ -13,16 +18,22 @@ var prev_dir = Vector2(1,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	tailGenerateWaitTime += delta
+	if (tailGenerateWaitTime > tailGenerateDuration):
+		tailGenerateWaitTime = 0
+		tailGenerateTimer.wait_time = tailGenerateDuration
+		tailGenerateTimer.start()
+			
 	move()
+	move_and_slide()
 	
 func move():
-	var head_pos = get_node("Head").position
-	get_node("Head").velocity = move_direction.direction * move_speed
+	velocity = move_direction.direction * move_speed
 	
 	var dir_change = false
 	if (prev_dir != move_direction.direction):
@@ -30,5 +41,17 @@ func move():
 		dir_change = true
 	if (dir_change == true):
 		for i in tails:
-			get_node(i).add_move_dir_to_tail(head_pos, move_direction.direction)
+			i.add_move_dir_to_tail(position, move_direction.direction)
+			
+func add_tails():
+	if (tails.size() < 6):
+		var new_tail = tail.instantiate()
+		new_tail.move_speed = move_speed
+		get_parent().add_child(new_tail)
+		tails.append(new_tail)
+
+
+
+func _on_tail_generate_timer_timeout():
+	add_tails()
 	
--- Scripts/Enemy/Enemy_Type101_Tail.gd
+++ Scripts/Enemy/Enemy_Type101_Tail.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-var cur_dir = Vector2(0,0)
+var cur_dir = Vector2(1,0)
 var directions = []
 var pos_array = []
+
+var move_speed = 100
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,17 +13,25 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	velocity = cur_dir * get_parent().move_speed
+func _physics_process(delta):
+	#print_debug(directions.size())
+	print_debug(pos_array.size())
+	
 	if (directions.size() > 0):
 		if (position == pos_array[0]):
 			cur_dir = directions[0]
 			remove_move_dir_from_tail()
+			
+	print_debug(cur_dir)		
+	velocity = cur_dir * move_speed
+	
+	move_and_slide()
 	
 func remove_move_dir_from_tail():
 	directions.pop_front()
 	pos_array.pop_front()
 	
 func add_move_dir_to_tail(head_pos, dir):
-	directions.append(head_pos)
-	pos_array.append(dir)
+	directions.append(dir)
+	pos_array.append(head_pos)
+
