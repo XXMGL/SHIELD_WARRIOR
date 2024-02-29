ce4fe38f5e620836e5036753ddd0763f036f3991
MaskMan-DU <duyiran19385@gmail.com>
2024-02-28 20:17:31 -0500

    Finish most part of Boss2

--- Scripts/Enemy/Enemy_Type101.gd
+++ Scripts/Enemy/Enemy_Type101.gd
@@ -1,30 +1,68 @@
 extends CharacterBody2D
 
+# 尾巴
 var tail = preload("res://TSCN/Enemy/Enemies/enemy_type_101_Tail.tscn")
 @onready var tailGenerateTimer = $Tail_Generate_Timer
 var tailGenerateDuration = 0.8
-
-@export var move_direction = Node2D
-@export var move_speed = 100
 var tails = []
 
+# 子弹
+@onready var Bullet = preload("res://TSCN/Bullet/bullet_2.tscn")
+@onready var ShootTimer = $ShootTimer
+@export var ShootDuration = 0.5
+var shooterCalmDown = true
+
+# 子弹发射器
+@onready var bulletSpawner1 = $BulletSpawner1
+@onready var bulletSpawner2 = $BulletSpawner2
+@onready var bulletSpawner3 = $BulletSpawner3
+@onready var bulletSpawner4 = $BulletSpawner4
+@onready var bulletSpawner5 = $BulletSpawner5
+@onready var bulletSpawner6 = $BulletSpawner6
+@onready var bulletSpawner7 = $BulletSpawner7
+@onready var bulletSpawner8 = $BulletSpawner8
+
+var bulletSpawners = []
+
+# 移动
+@export var move_direction = Node2D
+@export var move_speed = 100
 @export var group_name : String
-@export var ShootDuration = 2.0
 
-@export var MaxHealth = 1000
+# 生命
+var Health = 1
+@export var MaxHealth = 200
+var canBeHurt = false
 
-var prev_dir = Vector2(1,0)
+# Boss 状态
+enum ShootMode{SM1, SM2, Mad, Weak, Die}
+@export var SM = ShootMode.SM1
+var isWeak = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	bulletSpawners = [bulletSpawner1, bulletSpawner2,bulletSpawner3,bulletSpawner4,bulletSpawner5,bulletSpawner6,bulletSpawner7,bulletSpawner8]
 	tailGenerateTimer.wait_time = tailGenerateDuration
+	ShootTimer.wait_time = ShootDuration
+	Health = MaxHealth
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-			
+	TailsCheck()
+	ChangeTailsState()
 	move()
+	BossState()
+	
+	if (!canBeHurt):
+		Health = MaxHealth
+		
+		
+	if (Health <= 0):
+		SM = ShootMode.Die
+		
 	move_and_slide()
 	
 func move():
@@ -37,9 +75,92 @@ func add_tails():
 		new_tail.group_name = group_name
 		get_parent().add_child(new_tail)
 		tails.append(new_tail)
-
-
+	#print_debug(tails)
 
 func _on_tail_generate_timer_timeout():
 	add_tails()
 	
+	
+func BossState():
+	match (SM):
+		ShootMode.SM1:
+			pass
+		ShootMode.SM2:
+			move_speed = 150
+			ChangeTailsMoveSpeed(move_speed)
+			pass
+		ShootMode.Mad:
+			pass
+		ShootMode.Weak:
+			canBeHurt = true
+			move_speed = 100
+			if (shooterCalmDown):
+				ShootTimer.start()
+				shooterCalmDown = false
+			ChangeTailsMoveSpeed(move_speed)
+			pass
+		ShootMode.Die:
+			ShootTimer.stop()
+			move_speed = 0
+			ChangeTailsMoveSpeed(move_speed)
+			pass
+
+func ChangeTailsMoveSpeed(move_speed):
+	if (tails != null):
+		for i in tails:
+			i.move_speed = move_speed
+			
+func TailsCheck():
+	if (tails != null && !isWeak):
+		var diedTailCount = 0
+		# 查看Tail中的死亡数量
+		for i in tails:
+			if (i.SM == i.ShootMode.Die):
+				diedTailCount += 1
+		print_debug(diedTailCount)
+		# Tail死亡超过两个，进入状态2
+		if (diedTailCount >= 2):
+			SM = ShootMode.SM2
+			#print_debug(SM)
+		# Tail死亡超过四个，进入癫狂状态
+		if (diedTailCount >= 4):
+			SM = ShootMode.Mad
+			#print_debug(SM)
+		# Tail全部死亡，进入虚弱状态
+		if (diedTailCount == 6):
+			SM = ShootMode.Weak
+			isWeak = true
+			#print_debug(SM)
+			
+func ChangeTailsState():
+	if (tails != null):
+		for i in tails:
+			if (SM == ShootMode.SM1):
+				i.SM = i.ShootMode.SM1
+			if (SM == ShootMode.SM2):
+				i.SM = i.ShootMode.SM2
+			if (SM == ShootMode.Mad):
+				i.SM = i.ShootMode.Mad
+
+
+func _EnemyDetection():
+	pass
+	
+func _BossDetection():
+	pass
+
+
+func _on_shoot_timer_timeout():
+	ShootBullets()
+	shooterCalmDown = true
+	pass # Replace with function body.
+
+func ShootBullets():
+	for i in bulletSpawners:
+		var bullet = Bullet.instantiate()
+		get_parent().add_child(bullet)
+		var bulletDirection = (i.global_position - global_position).normalized()
+
+		bullet.position = i.global_position
+		bullet.rotation = atan2(bulletDirection.x,bulletDirection.y)
+		bullet.MoveDirection = bulletDirection
--- Scripts/Enemy/Enemy_Type101_Tail.gd
+++ Scripts/Enemy/Enemy_Type101_Tail.gd
@@ -1,30 +1,49 @@
 extends CharacterBody2D
 
+# 子弹
 @onready var Bullet = preload("res://TSCN/Bullet/bullet_2.tscn")
+@onready var chaseBullet = preload("res://TSCN/Bullet/bullet_1.tscn")
 @onready var ShootTimer = $ShootTimer
 
+# 子弹发射器
 @onready var bulletSpawner1 = $BulletSpawner1
 @onready var bulletSpawner2 = $BulletSpawner2
 @onready var bulletSpawner3 = $BulletSpawner3
 @onready var bulletSpawner4 = $BulletSpawner4
 
-enum ShootMode{SM1,SM2,Mad}
-@export var SM = ShootMode.SM1
+var bulletSpawner4RotateDir = 1
 
+# Boss 状态
+enum ShootMode{SM1, SM2, Mad, Die}
+@export var SM = ShootMode.SM1
+@export var SM1_ShootDuraton = 1.5
+@export var SM2_ShootDuraton = 0.2
+@export var SMM_ShootDuraton = 0.5
+
+
+# Boss移动
 var move_speed = 100
 @export var move_direction = Node2D
 var group_name : String
 
+# Boss生命
+@export var Health = 50
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ShootTimer.wait_time = SM1_ShootDuraton
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	
 	velocity = move_direction.direction * move_speed
 	
 	BossState()
+	
+	if (Health <= 0):
+		SM = ShootMode.Die
 	
 	move_and_slide()
 	
@@ -32,41 +51,101 @@ func _physics_process(delta):
 func BossState():
 	match (SM):
 		ShootMode.SM1:
+			# 改变射击速度
+			ShootTimer.wait_time = SM1_ShootDuraton
+			
+			
 			pass
 		ShootMode.SM2:
-			var rotateSpeed = 0.2
-			var rotateDir=0
+			# 改变射击速度
+			ShootTimer.wait_time = SM2_ShootDuraton
 			
-			print_debug(bulletSpawner4.rotation_degrees)
+			# 调整射击孔
+			var rotateSpeed = 0.02
+		
+			#print_debug(bulletSpawner4.rotation_degrees)
+			
 			if (bulletSpawner4.rotation_degrees >= 180):
-				rotateDir = -1
+				bulletSpawner4RotateDir = -1
 			elif(bulletSpawner4.rotation_degrees <= 0):
-				rotateDir = 1
+				bulletSpawner4RotateDir = 1
 			
-			bulletSpawner4.rotation += rotateSpeed * rotateDir
-
+			bulletSpawner4.rotation += rotateSpeed * bulletSpawner4RotateDir
 			pass
 		ShootMode.Mad:
+			# 改变射击速度
+			ShootTimer.wait_time = SMM_ShootDuraton
+			
+			var rotateSpeed = 0.01
+		
+			#print_debug(bulletSpawner4.rotation_degrees)
+			
+			if (bulletSpawner4.rotation_degrees >= 180):
+				bulletSpawner4RotateDir = -1
+			elif(bulletSpawner4.rotation_degrees <= 0):
+				bulletSpawner4RotateDir = 1
+			
+			bulletSpawner4.rotation += rotateSpeed * bulletSpawner4RotateDir
 			pass
+		ShootMode.Die:
+			ShootTimer.stop()
+			pass
+		
 
 func ShootBullet():
 	match (SM):
 		ShootMode.SM1:
+			#print_debug("1")
+			var bullet1 = Bullet.instantiate()
+			get_parent().add_child(bullet1)
+			bullet1.position = bulletSpawner1.global_position
+			
+			var bullet2 = Bullet.instantiate()
+			get_parent().add_child(bullet2)
+			bullet2.position = bulletSpawner2.global_position
+			
+			var bullet3 = Bullet.instantiate()
+			get_parent().add_child(bullet3)
+			bullet3.position = bulletSpawner3.global_position
 			pass
 		ShootMode.SM2:
 			var bullet = Bullet.instantiate()
 			get_parent().add_child(bullet)
-			var bulletDirection = (get_node("BulletSpawner4/ShootDirection").global_position - get_node("BulletSpawner4").global_position).normalized()
+			var bulletDirection = (get_node("BulletSpawner4/ShootDirection").global_position - bulletSpawner4.global_position).normalized()
 
-			bullet.position = get_node("BulletSpawner4").global_position
+			bullet.position = bulletSpawner4.global_position
 			bullet.rotation = atan2(bulletDirection.x,bulletDirection.y)
 			bullet.MoveDirection = bulletDirection
 			
 			pass
 		ShootMode.Mad:
+			var bullet1 = chaseBullet.instantiate()
+			get_parent().add_child(bullet1)
+			bullet1.position = bulletSpawner1.global_position
+			
+			var bullet3 = chaseBullet.instantiate()
+			get_parent().add_child(bullet3)
+			bullet3.position = bulletSpawner3.global_position
+			
+			var bullet = Bullet.instantiate()
+			get_parent().add_child(bullet)
+			var bulletDirection = (get_node("BulletSpawner4/ShootDirection").global_position - bulletSpawner4.global_position).normalized()
+
+			bullet.position = bulletSpawner4.global_position
+			bullet.rotation = atan2(bulletDirection.x,bulletDirection.y)
+			bullet.MoveDirection = bulletDirection
+			
+			pass
+		ShootMode.Die:
 			pass
 
 
 
 func _on_shoot_timer_timeout():
+	#print_debug("1")
 	ShootBullet()
+
+
+func _EnemyDetection():
+	pass
+	
