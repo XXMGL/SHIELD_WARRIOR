2166f5a34410822648e4dc0fbd1961d96f844b1a
DearJohnn <sun.yuch@northeastern.edu>
2024-02-29 19:33:34 -0500

    Edit StartScene

--- TSCN/Scene/StartScene.gd
+++ TSCN/Scene/StartScene.gd
@@ -2,11 +2,19 @@ extends CanvasLayer
 
 @onready var CharacterAnimation = $AnimatedSprite2D
 @onready var EnemyAnimation = $AnimatedSprite2D2
+@onready var EnemyAnimation2 = $AnimatedSprite2D3
+@onready var EnemyAnimation3 = $AnimatedSprite2D4
+@onready var EnemyAnimation4 = $AnimatedSprite2D5
+@onready var EnemyAnimation5 = $AnimatedSprite2D6
 
 func _ready():
 	_HidePlayer()
 	CharacterAnimation.play("idle")
 	EnemyAnimation.play("idle")
+	EnemyAnimation2.play("idle")
+	EnemyAnimation3.play("idle")
+	EnemyAnimation4.play("idle")
+	EnemyAnimation5.play("idle")
 	pass
 
 
