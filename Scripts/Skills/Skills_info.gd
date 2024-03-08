4bcf32362a43a700b1564d92e61609845b5add28
XXMGL <1056181445@qq.com>
2024-03-07 22:37:13 -0500

    Create Skills_info.gd

---
+++ Scripts/Skills/Skills_info.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var G_Skills_info = {
+	"G_Skill_1":{},
+	"G_Skill_2":{},
+}
+var B_Skills_info = {
+	"B_Skill_1":{"Lv 1 Guardian Shield":"Guardian Shield Lv1 Content",
+	"Lv 2 Guardian Shield":"Guardian Shield Lv2 Content",
+	"Lv 3 Guardian Shield":"Guardian Shield Lv3 Content"},
+	"B_Skill_2":{"Lv 1 Mystic Familiar":"Mystic Familiar Lv1 Content",
+	"Lv 2 Mystic Familiar":"Mystic Familiar Lv2 Content",
+	"Lv 3 Mystic Familiar":"Mystic Familiar Lv3 Content",
+	"Lv 4_1 Mystic Familiar":"Mystic Familiar Lv4_1 Content",
+	"Lv 4_2 Mystic Familiar":"Mystic Familiar Lv4_2 Content",
+	"Lv 5_1 Mystic Familiar":"Mystic Familiar Lv5_1 Content",
+	"Lv 5_2 Mystic Familiar":"Mystic Familiar Lv5_2 Content"}
+}
+var L_Skills_info = {
+	"L_Skill_1":{"Lv 1 Shards Shoot":"Shards Shoot Lv1 Content",
+	"Lv 2 Shards Shoot":"Shards Shoot Lv2 Content",
+	"Lv 3 Shards Shoot":"Shards Shoot Lv3 Content"},
+	"L_Skill_2":{"Lv 1 Reposition":"Reposition Lv1 Content",
+	"Lv 2 Reposition":"Shards Shoot Lv2 Content",
+	"Lv 3 Reposition":"Reposition Lv3 Content"},
+	"L_Skill_3":{"Lv 1 WingMan":"WingMan Lv1 Content",
+	"Lv 2 WingMan":"WingMan Lv2 Content",
+	"Lv 3 WingMan":"WingMan Lv3 Content"}
+}
+
