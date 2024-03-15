ededb8d48b1fdd4d527fdcdfb321506f97000e3f
MaskMan-DU <duyiran19385@gmail.com>
2024-03-14 21:35:45 -0400

    1

---
+++ Scripts/Enemy/EnemyInfor.gd
@@ -0,0 +1,41 @@
+extends Node
+
+var enemy_data = {
+	"Normal":{
+			"Enemy 1":{
+				"Path": "res://TSCN/Enemy/Enemies/enemy_type_1.tscn",
+				"EXP":4,
+			},
+			
+			"Enemy 2":{
+				"Path": "res://TSCN/Enemy/Enemies/enemy_type_2.tscn",
+				"EXP":2,
+			},
+			
+			"Enemy 3":{
+				"Path": "res://TSCN/Enemy/Enemies/enemy_type_3.tscn",
+				"EXP":2,
+			},
+			
+			"Enemy 4":{
+				"Path": "res://TSCN/Enemy/Enemies/enemy_type_4.tscn",
+				"EXP":4,
+			},
+			
+			"Enemy 5":{
+				"Path": "res://TSCN/Enemy/Enemies/enemy_type_6.tscn",
+				"EXP":4,
+			},
+		},
+	"Elite":{
+		"Enemy 1":{
+				"Path": "res://TSCN/Enemy/Enemies/enemy_type_5.tscn",
+				"EXP":5,
+			},
+			
+		"Enemy 2":{
+				"Path": "res://TSCN/Enemy/Enemies/enemy_type_7.tscn",
+				"EXP":8,
+			},
+	}
+}
